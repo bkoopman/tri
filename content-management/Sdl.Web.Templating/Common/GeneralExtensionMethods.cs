a605e4c6f05bcc5428cae8672e2519dda30e9c5c
Bart Koopman <bkoopman@sdl.com>
2014-09-02 10:42:42 +0200

    Added source of Template Building Blocks

---
+++ content-management/Sdl.Web.Templating/Common/GeneralExtensionMethods.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace Sdl.Web.Tridion.Common
+{
+    public static class GeneralExtensionMethods
+    {
+        public static string GetIso8601Date(this DateTime date)
+        {
+            return date > DateTime.MinValue ? date.ToString("s", CultureInfo.InvariantCulture) + "Z" : null;
+        }
+    }
+}
---
+++ content-management/Sdl.Web.Templating/Common/Utility.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Tridion.ContentManager.CommunicationManagement;
+using Tridion.ContentManager.ContentManagement;
+
+namespace Sdl.Web.Tridion.Common
+{
+    /// <summary>
+    /// Static Helper methods
+    /// </summary>
+    public static class Utility
+    {
+        public static string GetKeyFromTaxonomy(Category taxonomy)
+        {
+            var key = taxonomy.XmlName;
+            return key.Substring(0, 1).ToLower() + key.Substring(1);
+        }
+
+        public static string GetKeyFromTemplate(ComponentTemplate template)
+        {
+            var key = Regex.Replace(template.Title, @"[\[\]\s\.]", "");
+            return key.Substring(0, 1).ToLower() + key.Substring(1);
+        }
+
+        public static string GetKeyFromSchema(Schema schema)
+        {
+            var key = schema.RootElementName;
+            if (String.IsNullOrEmpty(key))
+            {
+                key = Regex.Replace(schema.Title.Trim(), @"[^A-Za-z0-9.]+", "");
+            }
+            return key.Substring(0, 1).ToLower() + key.Substring(1);
+        }
+    }
+}
