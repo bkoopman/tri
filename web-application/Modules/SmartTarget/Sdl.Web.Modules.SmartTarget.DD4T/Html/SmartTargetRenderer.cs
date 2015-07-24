741bde049b77e214217cc8e5038ab8025a2c0dcc
Jeroen Suurd <jeroen.suurd@tahzoo.com>
2015-07-24 13:07:23 -0700

    Extracted rendering of promotions to separate method

--- web-application/Modules/SmartTarget/Sdl.Web.Modules.SmartTarget.DD4T/Html/SmartTargetRenderer.cs
+++ web-application/Modules/SmartTarget/Sdl.Web.Modules.SmartTarget.DD4T/Html/SmartTargetRenderer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Text;
 using System.Web.Mvc;
 using Sdl.Web.Common.Models;
@@ -14,74 +15,81 @@ namespace Sdl.Web.Modules.SmartTarget.DD4T.Html
     {
         private const string _defaultSiteEditTag = "span";
 
-        public MvcHtmlString RenderSmartTargetQuery(IRegion region, HtmlHelper helper, string siteEditTag = _defaultSiteEditTag)
+        public MvcHtmlString RenderSmartTargetQuery(SmartTargetRegion region, HtmlHelper helper, string siteEditTag = _defaultSiteEditTag)
         {
             var siteEditOpenTag = String.Format("<{0}>", siteEditTag);
             var siteEditCloseTag = String.Format("</{0}>", siteEditTag);
 
-            if (region is SmartTargetRegion)
+            var xmpMarkup = new StringBuilder();
+            var isPreview = WebRequestContext.IsPreview;
+            if (isPreview)
             {
-                var smartTargetRegion = region as SmartTargetRegion;
-
-                var xmpMarkup = new StringBuilder();
-                var isPreview = WebRequestContext.IsPreview;
-                if (isPreview)
-                {
-                    xmpMarkup.AppendLine(siteEditOpenTag);
-                    xmpMarkup.AppendLine(smartTargetRegion.XpmMarkup);
-                }
-                foreach (var item in smartTargetRegion.Items)
-                {
-                    if (item is SmartTargetPromotion)
-                    {
-                        var promotion = item as SmartTargetPromotion;
-                        if (promotion.IsVisible)
-                        {
-                            if (isPreview)
-                            {
-                                xmpMarkup.AppendLine(siteEditOpenTag);
-                                xmpMarkup.AppendLine(promotion.XpmMarkup);
-                            }
-                            foreach (var promotionItem in promotion.Items)
-                            {
-                                if (promotionItem.IsVisible)
-                                {
-                                    string renderedContent = RenderEntity(promotionItem.Entity, helper).ToHtmlString();
-                                    if (promotion is SmartTargetExperiment)
-                                    {
-                                        var experiment = promotion as SmartTargetExperiment;
-                                        string renderedContentWithTrackedLinks = SmartTargetAnalytics.AddTrackingToLinks(renderedContent, experiment);
-                                        xmpMarkup.AppendLine(renderedContentWithTrackedLinks);
-
-                                        SmartTargetAnalytics.TrackView(experiment);
-                                        SmartTargetAnalytics.SaveExperimentCookies(experiment);
-                                    }
-                                    else
-                                    {
-                                        xmpMarkup.AppendLine(renderedContent);
-                                    }
-                                }
-                            }
-                            if (isPreview)
-                            {
-                                xmpMarkup.AppendLine(siteEditCloseTag);
-                            }
-                        }
-                    }
-                    else
-                    {
-                        // Fallback content
-                        xmpMarkup.AppendLine(RenderEntity(item, helper).ToHtmlString());
-                    }
-                }
-                if (isPreview)
-                {
-                    xmpMarkup.AppendLine(siteEditCloseTag);
-                }                
-
-                return MvcHtmlString.Create(xmpMarkup.ToString());
+                xmpMarkup.AppendLine(siteEditOpenTag);
+                xmpMarkup.AppendLine(region.XpmMarkup);
             }
-            return null;
+            if (region.HasSmartTargetContent)
+            {
+                foreach (var promotion in region.Items.Cast<SmartTargetPromotion>().Where(p => p.IsVisible))
+                {
+                    if (isPreview)
+                    {
+                        xmpMarkup.AppendLine(siteEditOpenTag);
+                        xmpMarkup.AppendLine(promotion.XpmMarkup);
+                    }
+                    foreach (var item in promotion.Items.Where(i => i.IsVisible))
+                    {
+                        xmpMarkup.AppendLine(RenderPromotionContent(promotion, helper));
+                    }
+                    if (isPreview)
+                    {
+                        xmpMarkup.AppendLine(siteEditCloseTag);
+                    }
+                }
+            }
+            else
+            {
+                // Fallback content
+                foreach (var item in region.Items)
+                {
+                    xmpMarkup.AppendLine(RenderEntity(item, helper).ToHtmlString());
+                }
+            }
+            if (isPreview)
+            {
+                xmpMarkup.AppendLine(siteEditCloseTag);
+            }
+
+            return MvcHtmlString.Create(xmpMarkup.ToString());
         }
+
+        public MvcHtmlString RenderSmartTargetPromotion(SmartTargetPromotion promotion, HtmlHelper helper)
+        {
+            return new MvcHtmlString(RenderPromotionContent(promotion, helper));
+        }
+
+        private string RenderPromotionContent(SmartTargetPromotion promotion, HtmlHelper helper)
+        {
+            var renderedContent = new StringBuilder();
+            foreach (var item in promotion.Items.Where(i => i.IsVisible))
+            {
+                renderedContent.AppendLine(RenderEntity(item.Entity, helper).ToHtmlString());
+            }
+
+            if (promotion is SmartTargetExperiment)
+            {
+                var experiment = promotion as SmartTargetExperiment;
+                string renderedContentWithTrackedLinks = SmartTargetAnalytics.AddTrackingToLinks(renderedContent.ToString(), experiment);
+
+                SmartTargetAnalytics.TrackView(experiment);
+                SmartTargetAnalytics.SaveExperimentCookies(experiment);
+
+                return renderedContentWithTrackedLinks;
+            }
+            else
+            {
+                return renderedContent.ToString();
+            }
+        }
+
     }
 }
